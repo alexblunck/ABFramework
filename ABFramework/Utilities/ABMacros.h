9c5f80a88fdab8a7489d52042a6bb67a726fe3ca
Alexander Blunck <ablfx@me.com>
2013-02-06 19:55:34 +0100

    CGSize logging macro

--- ABFramework/Utilities/ABMacros.h
+++ ABFramework/Utilities/ABMacros.h
@@ -26,6 +26,7 @@ LOGGING
 */
 #define ABLogNSString(string) NSLog(@"ABLogNSString -> %s = \"%@\"", #string, string)
 #define ABLogCGRect(rect) NSLog(@"ABLogCGRect -> %s = %@", #rect, NSStringFromCGRect(rect))
+#define ABLogCGSize(size) NSLog(@"ABLogCGSize -> %s = %@", #size, NSStringFromCGSize(size))
 #define ABLogBOOL(bool) NSLog(@"ABLogBOOL -> %s = %@", #bool, (bool) ? @"YES" : @"NO")
 #define ABLogInteger(integer) NSLog(@"ABLogInteger -> %s = %i", #integer, integer)
 #define ABLogFloat(float) NSLog(@"ABLogFloat -> %s = %f", #float, float)
