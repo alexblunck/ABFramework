94d58cb77fcf91490a9657cfaf0fb7f9347c623a
Alexander Blunck <ablfx@me.com>
2013-02-10 14:55:37 +0100

    Types file
    added block typedef's

---
+++ ABFramework/Utilities/ABTypes.h
@@ -0,0 +1,13 @@
+//
+//  ABTypes.h
+//  
+//
+//  Created by Alexander Blunck on 2/10/13.
+//
+//
+
+/*
+ BLOCKS
+ */
+typedef void (^ABBlockVoid) ();
+typedef void (^ABBlockIndex) (NSInteger index);
