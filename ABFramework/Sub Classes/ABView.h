3bd7eeaffcf30ad1b7e444935ffddc3ce39cf19b
Alexander Blunck <ablfx@me.com>
2013-02-10 14:55:09 +0100

    Custom UIView subclass with "userData" property

---
+++ ABFramework/Sub Classes/ABView.h
@@ -0,0 +1,15 @@
+//
+//  ABView.h
+//  
+//
+//  Created by Alexander Blunck on 2/10/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABView : UIView
+
+@property(nonatomic, strong) NSDictionary *userData;
+
+@end
---
+++ ABFramework/Sub Classes/ABView.m
@@ -0,0 +1,31 @@
+//
+//  ABView.m
+//  
+//
+//  Created by Alexander Blunck on 2/10/13.
+//
+//
+
+#import "ABView.h"
+
+@implementation ABView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
