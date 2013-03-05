4c32d3809ed405b3c56bf585f7a77c81c984ae09
Alexander Blunck <ablfx@me.com>
2013-03-05 03:01:03 +0100

    Added UIColor category
    - Hex String support e.g. #efefef

---
+++ ABFramework/Categories/UIColor+ABFramework.h
@@ -0,0 +1,16 @@
+//
+//  UIColor+ABFramework.h
+//  ABFramework
+//
+//  Created by Alexander Blunck on 3/4/13.
+//  Copyright (c) 2013 Serrano - Ablfx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (ABFramework)
+
+//Expects a hex string: #e2e2e2
++(UIColor*) colorWithHexString:(NSString*)hexString;
+
+@end
---
+++ ABFramework/Categories/UIColor+ABFramework.m
@@ -0,0 +1,36 @@
+//
+//  UIColor+ABFramework.m
+//  ABFramework
+//
+//  Created by Alexander Blunck on 3/4/13.
+//  Copyright (c) 2013 Serrano - Ablfx. All rights reserved.
+//
+//  Source: http://stackoverflow.com/questions/1560081/how-can-i-create-a-uicolor-from-a-hex-string
+//
+
+#import "UIColor+ABFramework.h"
+
+@implementation UIColor (ABFramework)
+
++(UIColor*) colorWithHexString:(NSString*)hexString
+{
+    NSString *colorString = [[hexString stringByReplacingOccurrencesOfString: @"#" withString: @""] uppercaseString];
+
+    CGFloat alpha = 1.0f;
+    CGFloat red   = [self colorComponentFrom: colorString start: 0 length: 2];
+    CGFloat green = [self colorComponentFrom: colorString start: 2 length: 2];
+    CGFloat blue  = [self colorComponentFrom: colorString start: 4 length: 2];
+    
+    return [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
+}
+
++(CGFloat) colorComponentFrom:(NSString*) string start:(NSUInteger) start length:(NSUInteger)length
+{
+    NSString *substring = [string substringWithRange:NSMakeRange(start, length)];
+    NSString *fullHex = length == 2 ? substring : [NSString stringWithFormat:@"%@%@", substring, substring];
+    unsigned hexComponent;
+    [[NSScanner scannerWithString:fullHex] scanHexInt:&hexComponent];
+    return hexComponent / 255.0;
+}
+
+@end
