014ae67b134f0c43bde8878a97a6ed2ef4273c3d
Alexander Blunck <alex@ablfx.com>
2013-07-30 12:08:01 +0200

    Added UIScreen Category: screenWidth / -Height / -Scale / KeyboardHeight / RetinaScreen methods

---
+++ ABFramework/iOS/Categories/UIScreen+ABFramework.h
@@ -0,0 +1,20 @@
+//
+//  UIScreen+ABFramework.h
+//  ABFramework
+//
+//  Created by Alexander Blunck on 7/23/13.
+//  Copyright (c) 2013 Ablfx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIScreen (ABFramework)
+
++(CGFloat) screenWidth;
++(CGFloat) screenHeight;
++(CGFloat) screenScale;
++(BOOL) retinaScreen;
+
++(CGFloat) keyboardHeight;
+
+@end
---
+++ ABFramework/iOS/Categories/UIScreen+ABFramework.m
@@ -0,0 +1,51 @@
+//
+//  UIScreen+ABFramework.m
+//  ABFramework
+//
+//  Created by Alexander Blunck on 7/23/13.
+//  Copyright (c) 2013 Ablfx. All rights reserved.
+//
+
+#import "UIScreen+ABFramework.h"
+
+@implementation UIScreen (ABFramework)
+
++(CGFloat) screenWidth
+{
+    return [[self mainScreen] bounds].size.width;
+}
+
++(CGFloat) screenHeight
+{
+    return [[self mainScreen] bounds].size.height;
+}
+
++(CGFloat) screenScale
+{
+    return [[self mainScreen] scale];
+}
+
++(BOOL) retinaScreen
+{
+    return ([self screenScale] >= 2.0f);
+}
+
++(CGFloat) keyboardHeight
+{
+    BOOL portrait = UIInterfaceOrientationIsPortrait([[UIApplication sharedApplication] statusBarOrientation]);
+    
+    //iPad
+    if (IS_IPAD)
+    {
+        return (portrait) ? 264.0f : 352.0f;
+    }
+    //iPhone
+    else
+    {
+        return (portrait) ? 216.0f : 162.0f;
+    }
+    
+    return 0;
+}
+
+@end
